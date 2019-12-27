438e4998e836f10d73c5d2022e32d74a400d39ee
francinvaldo <francinvaldo.wise@gmail.com>
2019-12-27 18:13:03 -0200

    server

--- view/server.py
+++ view/server.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Docker jenkis - MLOps - Francinvaldo - python"
+    return "Docker jenkis - MLOps -Francinvaldo"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
