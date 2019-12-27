e8a96e5077a2a4942d51a17b99884459c0799158
francinvaldo <francinvaldo.wise@gmail.com>
2019-12-27 14:15:34 -0200

    jenkins docker

--- server.py
+++ server.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Docker file"
+    return "Docker file-wise"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
